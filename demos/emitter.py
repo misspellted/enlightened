0a4b12a28ec6a571697ff37ee841c8c60f43f725
James Goodrich <jdgoodrichjr@gmail.com>
2021-02-28 20:05:17 -0600

    Adds ray emissions
    Went through a few variations... and kinda sad I didn't commit each
    iteration. But, each iteration can eventually be recreated from the
    changes involved in this one (although, some might have to be commented
    out to affect):
    
    * Added velocity (vx=1, vy=1)
    * Randomized velocity
    * Added ray "life"
    * Removed "dead" rays
    * Added trails
    * Added erasing all but last trail
    
    Next set of changes would include "clearing the camera sensor" after
    some predetermined amount of time. But that's for next weekend.

--- demos/emitter.py
+++ demos/emitter.py
@@ -1,8 +1,43 @@
 
 from demos import PyGameDemo
+from random import random
 import pygame
 
+class Ray:
+  MAXIMUM_LIFE = 2000
+
+  def __init__(self, position):
+    self.lastPosition = None
+    self.position = position
+    self.velocity = (random(), random())
+    self.rayColor = (127, 127, 0)
+    self.life = int(random() * Ray.MAXIMUM_LIFE)
+    self.trail = list()
+
+  def update(self, space):
+    if 0 < self.life:
+      self.lastPosition = self.position
+      px, py = self.position
+      vx, vy = self.velocity
+      if not 0 <= px < space[0]:
+        vx *= -1
+      if not 0 <= py < space[1]:
+        vy *= -1
+      px += vx
+      py += vy
+      self.position = (px, py)
+      self.velocity = (vx, vy)
+      self.life -= 1
+
+  def draw(self, surface):
+    pygame.draw.line(surface, self.rayColor, self.lastPosition, self.position)
+    self.trail.append((self.lastPosition, self.position))
+
 class EmittingDemo(PyGameDemo):
+  MAXIMUM_RAYS = 16
+  RAYS_PER_EMIT = MAXIMUM_RAYS >> 2
+  EMIT_COOLDOWN = RAYS_PER_EMIT << 8
+
   def __init__(self):
     PyGameDemo.__init__(self)
     self.cursorPosition = None
@@ -16,6 +51,7 @@ class EmittingDemo(PyGameDemo):
     self.absorbing = False
     self.absorbColor = (0, 127, 0) # Green for 'grab'/absorb
     self.rays = list() # Track the rays in the scene.
+    self.emitCoolDown = 0
 
   def createWindow(self, length, height):
     PyGameDemo.createWindow(self, length, height)
@@ -41,6 +77,15 @@ class EmittingDemo(PyGameDemo):
         PyGameDemo.processEvent(self, event)
     else:
       PyGameDemo.processEvent(self, event)
+  
+  def emitRays(self, cursorPosition):
+    # Only have 16 rays maximum at a time.
+    # And only emit so many per time.
+    emitted = 0
+    while len(self.rays) < EmittingDemo.MAXIMUM_RAYS and emitted < EmittingDemo.RAYS_PER_EMIT:
+      self.rays.append(Ray(cursorPosition))
+      emitted += 1
+    return emitted
 
   def update(self):
     # Redraw where the last cursor was.
@@ -59,7 +104,7 @@ class EmittingDemo(PyGameDemo):
       self.lastCursorArea = self.window.subsurface(self.cursorRect).copy()
 
       # The cursor color is dependent on the state of the demo.
-      cursorColor = self.cursorColor # Default - TODO: Make the emitted rays bounce off the cursor.
+      cursorColor = self.cursorColor
       if self.emitting:
         cursorColor = self.emitColor
       elif self.absorbing:
@@ -67,8 +112,28 @@ class EmittingDemo(PyGameDemo):
 
       pygame.draw.circle(self.window, cursorColor, self.cursorPosition, self.cursorRadius, 1)
 
-      # TODO: Emit rays.
-      # TODO: Bounce rays.
-      # TODO: Grab rays.
+      # TODO: Absorb any rays hitting the cursor in absorb 'mode'.
+
+      # TODO: Bounce any existing rays hitting the cursor in bounce 'mode'.
+
+      # Emit new rays when the cursor is in emit 'mode'.
+      if self.emitting:
+        if self.emitCoolDown <= 0:
+          emitted = self.emitRays(self.cursorPosition)
+          if 0 < emitted:
+            self.emitCoolDown = EmittingDemo.EMIT_COOLDOWN
+            #print(f"Emitted {emitted} ray(s).")
+        else:
+          self.emitCoolDown -= 1
+
+      for ray in self.rays:
+        ray.update(self.window.get_size())
+
+        if 0 < len(ray.trail):
+          pygame.draw.line(self.window, (0, 0, 0), ray.trail[-1][0], ray.trail[-1][0])
+
+        ray.draw(self.window)
+
+      self.rays = [ray for ray in self.rays if 0 < ray.life]
 
     PyGameDemo.update(self)
